4a13e711def6bd1acc7a92d960a9b93ed137bc6f
Akshat Sharma <sharmaakshat212000@gmail.com>
2021-01-06 05:40:45 -0500

    Invite modal almost completed

--- portal/src/App.js
+++ portal/src/App.js
@@ -1,41 +1,41 @@
-import Main from './MainComponent'; 
-import './App.css';
-import {BrowserRouter} from 'react-router-dom';
-import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+import Main from "./MainComponent";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
   palette: {
-      primary: {
-          main: '#150578',
-      },
-      secondary: {
-          main: '#21DAF1'
-      },
-      common: {
-        white: '#ffffff',
-        black: '#000000',
-      },
-      success: {
-        main: '#01AE4B'
-      },
-      error: {
-        main: '#EB0000'
-      },
-      warning: {
-        main: '#F6AC00'
-      },
-  }
+    primary: {
+      main: "#2B56FF",
+    },
+    secondary: {
+      main: "#21DAF1",
+    },
+    common: {
+      white: "#ffffff",
+      black: "#000000",
+    },
+    success: {
+      main: "#01AE4B",
+    },
+    error: {
+      main: "#EB0000",
+    },
+    warning: {
+      main: "#F6AC00",
+    },
+  },
 });
 
 function App() {
   return (
     <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <Main />
-      </div>
-    </ThemeProvider>
-  </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Main />
+        </div>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
--- portal/src/component/dashboard/dashboard.js
+++ portal/src/component/dashboard/dashboard.js
@@ -8,11 +8,18 @@ import {
   Grid,
   Typography,
   TextField,
+  Divider,
 } from "@material-ui/core";
 import Navbar from "../navbar/navbar";
 import "./dashboard.css";
 import dashboardImg from "../../assets/dashboard-empty.png";
-import { PlusIcon, MailIcon } from "@primer/octicons-react";
+import {
+  PlusIcon,
+  MailIcon,
+  DeviceMobileIcon,
+  InfoIcon,
+  PaperAirplaneIcon,
+} from "@primer/octicons-react";
 
 function Dashboard(props) {
   const [invite, setInvitation] = useState(false);
@@ -34,7 +41,7 @@ function Dashboard(props) {
         visible={invite ? true : false}
         dismissCallback={closeInvitation}
         modalWidth={780}
-        modalHeight={500}
+        modalHeight={550}
       >
         <Grid container direction="column" alignItems="flex-start">
           <Grid item>
@@ -45,7 +52,7 @@ function Dashboard(props) {
               Invite your client using their Email or their Phone No.
             </Typography>
           </Grid>
-          <Grid item style={{ marginTop: 60, marginLeft: 40 }}>
+          <Grid item style={{ marginTop: 50, marginLeft: 50 }}>
             <Typography className="invite-input-text">
               Enter the email of the recipient in order to send an invite link
               to them
@@ -55,21 +62,115 @@ function Dashboard(props) {
             <Grid
               container
               direction="row"
-              alignItems="flex-end"
+              alignItems="center"
+              justify="center"
               style={{ marginTop: 10 }}
             >
               <Grid item>
-                <MailIcon size={25} />
+                <MailIcon size={30} />
               </Grid>
               <Grid item>
-                <TextField label="Email" variant="outlined" />
+                <TextField
+                  label="Email"
+                  variant="outlined"
+                  style={{ width: 650, marginLeft: 20 }}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item style={{ marginTop: 20, marginLeft: 200 }}>
+            <Grid
+              container
+              direction="row"
+              alignItems="center"
+              justify="center"
+              spacing={2}
+            >
+              <Grid item>
+                <Divider
+                  style={{ width: 125, height: 2.5, background: "#707070" }}
+                />
+              </Grid>
+              <Grid item>
+                <Typography
+                  style={{
+                    fontFamily: "Gilroy",
+                    fontSize: 18.5,
+                    opacity: "60%",
+                  }}
+                >
+                  OR
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Divider
+                  style={{
+                    width: 125,
+                    height: 2.5,
+                    background: "#707070",
+                  }}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item style={{ marginLeft: 50, marginTop: 20 }}>
+            <Typography className="invite-input-text">
+              Enter the cell number of the recipient in order to send them an
+              invite
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Grid
+              container
+              direction="row"
+              alignItems="center"
+              justify="center"
+              style={{ marginTop: 10 }}
+            >
+              <Grid item>
+                <DeviceMobileIcon size={30} />
+              </Grid>
+              <Grid item>
+                <TextField
+                  label="Phone"
+                  variant="outlined"
+                  style={{ width: 650, marginLeft: 20 }}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item style={{ marginTop: 20 }}>
+            <Grid container direction="row" spacing={3}>
+              <Grid item>
+                <InfoIcon className="invite-info-icon" size={20} />
+              </Grid>
+              <Grid item>
+                <Typography className="invite-info-text">
+                  The user will be invited though e-mail and SMS.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item style={{ marginTop: 40, marginLeft: 370 }}>
+            <Grid container direction="row" spacing={2}>
+              <Grid item>
+                <Button variant="contained">Cancel</Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  className="invite-send-button"
+                  endIcon={<PaperAirplaneIcon />}
+                >
+                  Send Invite
+                </Button>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
       </Modal>
     );
-  }
+  } // first time modal done
 
   if (true) {
     // from the database
