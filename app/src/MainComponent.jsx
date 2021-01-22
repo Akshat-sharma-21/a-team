a4a38060a885346b4e6801b0a1a7bae57b7484e2
Melvin. L. Abraham <melvinredvirus@gmail.com>
2021-01-22 08:55:32 +0530

    feat(app): Add routes for SignIn and SignUp

--- app/src/MainComponent.jsx
+++ app/src/MainComponent.jsx
@@ -1,12 +1,16 @@
-import { Switch, Route } from "react-router-dom";
 import Onboarding from "./components/Onboarding/Onboarding";
 import Roadmap from "./components/Roadmap/Roadmap";
+import SignIn from './components/account/SignIn/SignIn';
+import SignUp from "./components/account/SignUp/SignUp";
+import { Switch, Route } from "react-router-dom";
 
 function Main() {
   return (
     <Switch>
       <Route path="/onboard" component={Onboarding} />
       <Route path="/roadmap" component={Roadmap} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
     </Switch>
   );
 }
