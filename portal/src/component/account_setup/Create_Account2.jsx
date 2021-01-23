79dfb5f5a2875eed97c4ddadc248423a63eed6fb
itz-kartik <itzkartikforu@gmail.com>
2021-01-23 22:08:15 +0530

    account-setup components updated

--- portal/src/component/account_setup/Create_Account2.jsx
+++ portal/src/component/account_setup/Create_Account2.jsx
@@ -2,53 +2,68 @@ import React from "react";
 import { TextField, Button } from "@material-ui/core";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import ReallosModal from "../utilities/modal/modal";
+import Scaffold from "../utilities/scaffold/Scaffold";
 import cimage from "../../assets/cimage.png";
+import ReallosButton from "../utilities/reallos_button/ReallosButton";
 
 function Create_Account2() {
   return (
-    <ReallosModal visible="true" modalWidth="620px" modalHeight="391px">
-      <div className="container" style={container}>
-        <div className="item1" style={item1}>
-          <img
-            src={cimage}
-            alt="left component"
-            width="300px"
-            height="400px"
-          ></img>
-        </div>
+    <Scaffold>
+      <ReallosModal visible="true" modalWidth="620px" modalHeight="391px">
+        <div className="container" style={container}>
+          <div className="item1" style={item1}>
+            <img
+              src={cimage}
+              alt="left component"
+              width="300px"
+              height="400px"
+            ></img>
+          </div>
 
-        <div className="item2" style={item2}>
-          <h1
-            className="subitem1"
-            style={{ marginTop: "-380px", marginLeft: "280px" }}
-          >
-            Account Setup
-          </h1>
+          <div className="item2" style={item2}>
+            <h1
+              className="subitem1"
+              style={{ marginTop: "-380px", marginLeft: "280px" }}
+            >
+              Account Setup
+            </h1>
 
-          <TextField
-            style={subitem2}
-            className="subitem2"
-            variant="outlined"
-            label="Create Password"
-          ></TextField>
-          <TextField
-            style={subitem6}
-            className="subitem6"
-            variant="outlined"
-            label="Confirm Password"
-          ></TextField>
+            <TextField
+              style={subitem2}
+              className="subitem2"
+              variant="outlined"
+              label="Create Password"
+            ></TextField>
+            <TextField
+              style={subitem3}
+              className="subitem6"
+              variant="outlined"
+              label="Confirm Password"
+            ></TextField>
 
-          <div className="item3" style={item3}>
-            <Button style={subitem4} className="subitem4" variant="contained">
-              Back
-            </Button>
-            <Button style={subitem5} className="subitem5" variant="contained">
-              Next<ArrowForwardIcon variant="contained"></ArrowForwardIcon>
-            </Button>
+            <div
+              style={{
+                display: "flex",
+                flexdirection: "row",
+                marginLeft: "320px",
+                marginTop: "-80px",
+              }}
+            >
+              <div>
+                <ReallosButton>BACK</ReallosButton>
+              </div>
+
+              <div style={{ marginLeft: "10px" }}>
+                <ReallosButton primary>
+                  NEXT
+                  {/* <ArrowForwardIcon /> */}
+                </ReallosButton>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </ReallosModal>
+      </ReallosModal>
+    </Scaffold>
   );
 }
 
@@ -67,13 +82,6 @@ const item2 = {
   gridtemplaterows: "auto auto auto auto",
 };
 
-const item3 = {
-  display: "flex",
-  flexdirection: "row",
-  marginTop: "-150px",
-  marginLeft: "300px",
-};
-
 const subitem2 = {
   width: "275px",
   height: "10px",
@@ -81,27 +89,11 @@ const subitem2 = {
   left: "275px",
 };
 
-const subitem6 = {
+const subitem3 = {
   width: "275px",
   height: "10px",
   top: "-240px",
   left: "275px",
-};
-
-const subitem4 = {
-  top: "60px",
-  left: "90px",
-  width: "80px",
-  height: "40px",
-};
-
-const subitem5 = {
-  top: "60px",
-  left: "100px",
-  background: "linear-gradient(to left,#33ccff 0%,#2B44FF 90%)",
-  color: "white",
-  width: "80px",
-  height: "40px",
 };
 
 export default Create_Account2;
--- portal/src/component/account_setup/phone.jsx
+++ portal/src/component/account_setup/phone.jsx
@@ -1,113 +1,104 @@
 import React from "react";
-import { TextField, Button } from "@material-ui/core";
+import { TextField } from "@material-ui/core";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import ReallosModal from "../utilities/modal/modal";
+import ReallosButton from "../utilities/reallos_button/ReallosButton";
+import Scaffold from "../utilities/scaffold/Scaffold";
 import cimage from "../../assets/cimage.png";
 import verified from "../../assets/verified.png";
 
 function Email() {
   return (
-    <ReallosModal visible="true" modalWidth="620px" modalHeight="391px">
-      <div className="container" style={container}>
-        <div
-          className="item1"
-          style={{ marginTop: "-33px", marginLeft: "-45px" }}
-        >
-          <img
-            src={cimage}
-            alt="left component"
-            width="300px"
-            height="400px"
-          ></img>
-        </div>
-
-        <div className="item2" style={item2}>
-          <img
-            style={{ marginTop: "-380px", marginLeft: "380px" }}
-            src={verified}
-            alt="verified logo"
-            width="60px"
-            height="60px"
-          ></img>
-
-          <h2 style={{ marginLeft: "300px", marginTop: "-300px" }}>
-            Phone Verification
-          </h2>
-
-          <p style={{ marginLeft: "275px", marginTop: "-250px" }}>
-            A confirmation code has sent to your email . Please enter it to
-            proceed.
-          </p>
-
+    <Scaffold>
+      <ReallosModal visible="true" modalWidth="620px" modalHeight="391px">
+        <div className="container" style={container}>
           <div
-            className="item3"
-            style={{
-              marginLeft: "275px",
-              marginTop: "-170px",
-              display: "flex",
-              flexDirection: "row",
-            }}
+            className="item1"
+            style={{ marginTop: "-33px", marginLeft: "-45px" }}
           >
-            <TextField
-              variant="outlined"
-              style={(mystyle, { marginLeft: "-5px" })}
-            ></TextField>
-            <TextField
-              variant="outlined"
-              style={(mystyle, { marginLeft: "10px" })}
-            ></TextField>
-            <TextField
-              variant="outlined"
-              style={(mystyle, { marginLeft: "10px" })}
-            ></TextField>
-            <TextField
-              variant="outlined"
-              style={(mystyle, { marginLeft: "10px" })}
-            ></TextField>
+            <img
+              src={cimage}
+              alt="left component"
+              width="300px"
+              height="400px"
+            ></img>
           </div>
 
-          <p style={{ marginLeft: "280px", marginTop: "-100px" }}>
-            Did'nt get the code ?
-            <a href="#" style={{ color: "blue" }}>
-              resend it
-            </a>
-          </p>
+          <div className="item2" style={item2}>
+            <img
+              style={{ marginTop: "-390px", marginLeft: "380px" }}
+              src={verified}
+              alt="verified logo"
+              width="60px"
+              height="60px"
+            ></img>
 
-          <div
-            style={{
-              display: "flex",
-              flexdirection: "row",
-              marginLeft: "350px",
-              marginTop: "-60px",
-            }}
-          >
-            <Button
-              style={{ width: "50px", height: "35px" }}
-              variant="contained"
-            >
-              BACK
-            </Button>
+            <h2 style={{ marginLeft: "300px", marginTop: "-315px" }}>
+              Phone Verification
+            </h2>
 
-            <Button
-              variant="filled"
+            <p style={{ marginLeft: "275px", marginTop: "-270px" }}>
+              A confirmation code has sent to your email . Please enter it to
+              proceed.
+            </p>
+
+            <div
+              className="item3"
               style={{
-                background: "linear-gradient(to left,#33ccff 0%,#2B44FF 90%)",
-                marginLeft: "20px",
-                width: "80px",
-                height: "35px",
-                color: "white",
+                marginLeft: "275px",
+                marginTop: "-185px",
+                display: "flex",
+                flexDirection: "row",
               }}
             >
-              NEXT
-              <ArrowForwardIcon
-                style={{ color: "white" }}
-                variant="contained"
-              ></ArrowForwardIcon>
-            </Button>
+              <TextField
+                variant="outlined"
+                style={(mystyle, { marginLeft: "-5px" })}
+              ></TextField>
+              <TextField
+                variant="outlined"
+                style={(mystyle, { marginLeft: "10px" })}
+              ></TextField>
+              <TextField
+                variant="outlined"
+                style={(mystyle, { marginLeft: "10px" })}
+              ></TextField>
+              <TextField
+                variant="outlined"
+                style={(mystyle, { marginLeft: "10px" })}
+              ></TextField>
+            </div>
+
+            <p style={{ marginLeft: "280px", marginTop: "-115px" }}>
+              Did'nt get the code ?
+              <a href="#" style={{ color: "blue" }}>
+                resend it
+              </a>
+            </p>
+
+            <div
+              style={{
+                display: "flex",
+                flexdirection: "row",
+                marginLeft: "330px",
+                marginTop: "-60px",
+              }}
+            >
+              <div>
+                <ReallosButton>BACK</ReallosButton>
+              </div>
+
+              <div style={{ marginLeft: "10px" }}>
+                <ReallosButton primary>
+                  NEXT
+                  {/* <ArrowForwardIcon /> */}
+                </ReallosButton>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </ReallosModal>
+      </ReallosModal>
+    </Scaffold>
   );
 }
 
