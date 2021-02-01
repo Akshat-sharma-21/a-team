2746e3ec16684d6f8ac7f22afbdd9c442bc03007
Akhil Sanker <reekithak@gmail.com>
2021-02-01 13:10:34 +0530

    PreApproval Screens

--- app/src/MainComponent.jsx
+++ app/src/MainComponent.jsx
@@ -5,6 +5,8 @@ import Document from "./components/Documents/Documents";
 import Document2 from "./components/Documents/Documents2";
 import Signin from "./components/account/SignIn/SignIn";
 import Signup from "./components/account/SignUp/SignUp";
+import PreApproval from "./components/PreApproval/PreApproval";
+import PreApproval2 from "./components/PreApproval2/PreApproval2";
 
 function Main() {
   return (
@@ -15,6 +17,8 @@ function Main() {
       <Route path="/documents2" component={Document2} />
       <Route path="/Signin" component={Signin} />
       <Route path="/Signup" component={Signup} />
+      <Route path="/preapproval" component={PreApproval} />
+      <Route path="/preapproval2" component={PreApproval2} />
     </Switch>
   );
 }
