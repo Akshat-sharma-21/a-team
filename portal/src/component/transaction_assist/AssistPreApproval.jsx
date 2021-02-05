44c8e6f7bae524532b9e82bacdd869969b623bca
Melvin. L. Abraham <melvinredvirus@gmail.com>
2021-02-05 16:48:10 +0530

    feat(portal): Refactor & update Transaction Assist
    - Update styling of the page components
    - Add `AssistAccordion` component
    - Add and remove assets: `doc_icon`
    - Refactor transaction assist components

---
+++ portal/src/component/transaction_assist/AssistPreApproval.jsx
@@ -0,0 +1,293 @@
+import React from 'react';
+import DocIcon from "../../assets/doc_icon.png";
+import DocGrayscaleIcon from "../../assets/doc_icon_greyscale.png";
+import AssistAccordion from './AssistAccordion';
+import { ReallosButton, SideDrawer } from '../utilities/core';
+
+import {
+  CheckCircleIcon,
+  CheckIcon,
+  DotFillIcon,
+  TagIcon,
+  PencilIcon,
+  CalendarIcon,
+  IssueOpenedIcon
+} from '@primer/octicons-react';
+
+import {
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Grid,
+  FormGroup,
+  TextField
+} from '@material-ui/core';
+
+/**
+ * Implements Pre-approval tasks, documents, etc.
+ * to be displayed in transaction assist
+ */
+class AssistPreApproval extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isAddTaskDrawerVisible: false,
+      isEntireTaskListVisible: false,
+      taskList: [
+        { name: "Task 1", date: "10 July", completed: true },
+        { name: "Task 2", date: "11 July", completed: true },
+        { name: "Task 3", date: "12 July", completed: false },
+        { name: "Task 4", date: "13 July", completed: false },
+        { name: "Task 5", date: "14 July", completed: false },
+        { name: "Task 6", date: "15 July", completed: false },
+        { name: "Task 7", date: "16 July", completed: false },
+      ],
+      documentsList: [
+        { name: 'Document 1', isFilled: true, link: '#' },
+        { name: 'Document 2', isFilled: true, link: '#' },
+        { name: 'Document 3', isFilled: false, link: '#' },
+        { name: 'Document 4', isFilled: false, link: '#' },
+        { name: 'Document 5', isFilled: false, link: '#' },
+        { name: 'Document 6', isFilled: true, link: '#' },
+      ]
+    }
+  }
+
+  /**
+   * Shows "Add Task" drawer.
+   */
+  showAddTaskDrawer() {
+    this.setState({
+      isAddTaskDrawerVisible: true
+    })
+  }
+
+  /**
+   * Hides "Add Task" drawer.
+   */
+  hideAddTaskDrawer() {
+    this.setState({
+      isAddTaskDrawerVisible: false
+    })
+  }
+
+  render() {
+    /**
+     * Any tasks beyond the split point will
+     * be hidden by default. User will have to
+     * explicitly click on "Show More" to show
+     * complete list of tasks.
+     * 
+     * @type {number}
+     */
+    const taskListSplitPoint = 5;
+    
+    return (
+      <>
+        <AssistAccordion
+          isStepComplete={false}
+          AccordionStepIcon={<CheckCircleIcon size={23} />}
+          title="Pre-approval"
+        >
+          {/* TASK LIST */}
+          <div className="assist-accordion-section-root">
+            <div className="assist-accordion-section-heading-group">
+              <h1>
+                Tasks
+              </h1>
+              <span className="assist-accordion-section-status">
+                {
+                  this.state.taskList.filter(taskItem => taskItem.completed).length
+                } of {
+                  this.state.taskList.length
+                } completed
+              </span>
+
+              <div style={{ marginLeft: 10 }}>
+                <ReallosButton
+                  primary
+                  onClick={() => this.showAddTaskDrawer()}
+                  className="add-task-button"
+                >
+                  Add New Task
+                </ReallosButton>
+              </div>
+            </div>
+
+            <div className="assist-task-list-root" style={{ marginTop: 10 }}>
+              {this.state.taskList.slice(0, this.state.isEntireTaskListVisible
+                ? this.state.taskList.length
+                : taskListSplitPoint
+              ).map((task) => {
+                return (
+                  <ListItem
+                    className={[
+                      'assist-task-list-item',
+                      task.completed ? 'assist-task-completed' : ''
+                    ].join(' ')}
+                  >
+                    <ListItemIcon className="assist-task-list-item-icon">
+                      {task.completed ? (
+                        <div style={{ color: '#0dd663' }}>
+                          <CheckIcon size={20} />
+                        </div>
+                      ) : (
+                        <div style={{ color: '#969696' }}>
+                          <DotFillIcon />
+                        </div>
+                      )}
+                    </ListItemIcon>
+
+                    <ListItemText>
+                      <span className="assist-task-list-item-date">{task.date}</span>
+                      <span className="assist-task-list-item-label">{task.name}</span>
+                    </ListItemText>
+                  </ListItem>
+                );
+              })}
+            </div>
+
+            {
+              (this.state.taskList.length > taskListSplitPoint) && (
+                <button
+                  className="assist-task-list-toggle-show link"
+                  onClick={() => this.setState((state) => ({
+                    isEntireTaskListVisible: !state.isEntireTaskListVisible
+                  }))}
+                >
+                  {this.state.isEntireTaskListVisible ? 'Show less' : 'Show more'}
+                </button>
+              )
+            }
+          </div>
+
+          {/* DOCUMENTS GRID */}
+          <div className="assist-accordion-section-root">
+            <div className="assist-accordion-section-heading-group">
+              <h1>
+                Documents
+              </h1>
+              <span className="assist-accordion-section-status">
+                {
+                  this.state.documentsList.filter(docItem => docItem.isFilled).length
+                } of {
+                  this.state.documentsList.length
+                } completed
+              </span>
+            </div>
+
+            <Grid container spacing={1} className="assist-doc-grid-root">
+              {this.state.documentsList.map((doc) => {
+                return (
+                  <Grid
+                    item xs={12} sm={6} md={4} lg={3}
+                    className="assist-doc-grid-item-root"
+                  >
+                    <a
+                      className="assist-doc-grid-item link-basic"
+                      href={doc.link}
+                    >
+                      <div className="assist-doc-grid-item-icon">
+                        <img
+                          src={doc.isFilled ? DocIcon : DocGrayscaleIcon}
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="assist-doc-grid-item-text-wrapper">
+                        <div className="assist-doc-grid-item-heading">
+                          {doc.name}
+                        </div>
+                        <div className="assist-doc-grid-item-subheading">
+                          {
+                            doc.isFilled
+                              ? 'Document is filled in'
+                              : 'Document yet to be filled'
+                          }
+                        </div>
+                      </div>
+                    </a>
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </div>
+        </AssistAccordion>
+
+        {/* ADD TASK MODAL */}
+        <SideDrawer
+          visible={this.state.isAddTaskDrawerVisible}
+          dismissCallback={() => this.hideAddTaskDrawer()}
+          title="Add a Task"
+          className="assist-add-task-drawer-root"
+        >
+          <div className="assist-add-task-drawer-subheading">
+            What is the task about?
+          </div>
+
+          <div className="assist-add-task-drawer-main">
+            <FormGroup row className="assist-add-task-drawer-form-field-group">
+              <TagIcon size={25} />
+
+              <div>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  label="Title"
+                />
+              </div>
+            </FormGroup>
+
+            <FormGroup row className="assist-add-task-drawer-form-field-group">
+              <PencilIcon size={25} />
+              <div>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  multiline
+                  rows={8}
+                  label="Description"
+                />
+              </div>
+            </FormGroup>
+
+            <FormGroup row className="assist-add-task-drawer-form-field-group">
+              <CalendarIcon size={25} className="tag-icon" />
+              <div>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  label="Date"
+                />
+              </div>
+            </FormGroup>
+
+            <FormGroup row className="assist-add-task-drawer-form-field-group">
+              <IssueOpenedIcon size={25} className="tag-icon" />
+              <div>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  label="Priority"
+                />
+              </div>
+            </FormGroup>
+          </div>
+
+          <div className="assist-add-task-drawer-footer">
+            <ReallosButton cta fullWidth onClick={() => this.hideAddTaskDrawer()}>
+              Back
+            </ReallosButton>
+
+            <ReallosButton cta fullWidth primary>
+              Add Task
+            </ReallosButton>
+          </div>
+        </SideDrawer>
+      </>
+    )
+  }
+}
+
+export default AssistPreApproval;
--- portal/src/component/transaction_assist/TransactionAssist.jsx
+++ portal/src/component/transaction_assist/TransactionAssist.jsx
@@ -1,149 +1,74 @@
 import { useState } from "react";
-import transactionImg from "../../assets/transaction-assist-first-time.png";
+import TransactionAssistIllustration from "../../assets/transaction-assist-first-time.png";
+import AssistPreApproval from "./AssistPreApproval";
+import AssistFindAgent from "./AssistFindAgent";
+import { PackageIcon } from "@primer/octicons-react";
+import "./TransactionAssist.css";
+
 import {
   ReallosModal,
   Scaffold,
   ReallosButton,
-  SideDrawer,
 } from "../utilities/core";
-import doc from "./doc.png";
-import "./TransactionAssist.css";
 
 import {
   Grid,
   Typography,
-  ExpansionPanel,
-  ExpansionPanelDetails,
-  ExpansionPanelSummary,
   Box,
-  Divider,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  FormGroup,
-  TextField,
 } from "@material-ui/core";
 
-import {
-  DotFillIcon,
-  TriangleDownIcon,
-  PackageIcon,
-  SearchIcon,
-  CheckCircleIcon,
-  CheckIcon,
-  PencilIcon,
-  TagIcon,
-  CalendarIcon,
-  IssueOpenedIcon,
-} from "@primer/octicons-react";
 
-function TransactionAssist(props) {
-  const [modalVisibile, setModal] = useState(true);
-  const [drawerVisible, setDrawer] = useState(false);
-  let [taskList, setTaskList] = useState([
-    { name: "Task1", date: "10 July", completed: true },
-    { name: "Task2", date: "11 July", completed: true },
-    { name: "Task3", date: "12 July", completed: false },
-    { name: "Task4", date: "13 July", completed: false },
-  ]);
+function TransactionAssist() {
+  const [isInitModalVisibile, setInitModalVisibility] = useState(true);
 
-  function addTaskDrawer() {
-    // fucntion to display the add task drawer
-    return (
-      <SideDrawer
-        visible={drawerVisible}
-        side={"right"}
-        dismissCallback={closeDrawer}
-        title="Add a Task"
-      >
-        <Typography className="task-subheading">
-          What is the task about?
-        </Typography>
-        <FormGroup row style={{ marginTop: "10%" }}>
-          <TagIcon size={30} className="tag-icon" />
-          <TextField variant="outlined" label="Title" className="form-fields" />
-        </FormGroup>
-        <FormGroup row style={{ marginTop: "5%" }}>
-          <PencilIcon size={30} className="tag-icon" />
-          <TextField
-            variant="outlined"
-            multiline
-            rows={8}
-            label="Description"
-            className="form-fields"
-          />
-        </FormGroup>
-        <FormGroup row style={{ marginTop: "5%" }}>
-          <CalendarIcon size={30} className="tag-icon" />
-          <TextField variant="outlined" label="Date" className="form-fields" />
-        </FormGroup>
-        <FormGroup row style={{ marginTop: "5%" }}>
-          <IssueOpenedIcon size={30} className="tag-icon" />
-          <TextField
-            variant="outlined"
-            label="Priority"
-            className="form-fields"
-          />
-        </FormGroup>
-        <div className="task-add-button-group" style={{ marginTop: "10%" }}>
-          <ReallosButton cta className="task-add-button-group-buttons">
-            Back
-          </ReallosButton>
-
-          <ReallosButton cta primary className="task-add-button-group-buttons">
-            Add Task
-          </ReallosButton>
-        </div>
-      </SideDrawer>
-    );
-  }
-
-  function openDrawer() {
-    setDrawer(true);
-  }
-
-  function closeDrawer() {
-    setDrawer(false);
-  }
-
-  function firstTimeModal() {
-    // function to display the first time modal
+  /**
+   * Renders first-time user modal.
+   */
+  const initModal = () => {
     return (
       <ReallosModal
-        visible={modalVisibile ? true : false}
+        visible={isInitModalVisibile}
         modalWidth={750}
-        modalHeight={500}
-        dismissCallback={closeModal}
+        dismissCallback={hideInitModal}
       >
         <Grid container direction="column" justify="center">
-          <Grid item xs={12} style={{ textAlign: "center" }}>
-            <img src={transactionImg} alt="" style={{ height: 290 }} />
+          <Grid item style={{ textAlign: "center" }}>
+            <img
+              src={TransactionAssistIllustration}
+              style={{ height: 290 }}
+              alt=""
+            />
           </Grid>
-          <Grid item xs={12} style={{ textAlign: "center" }}>
-            <Typography
+
+          <Grid item style={{
+            marginBottom: 20,
+            marginTop: 10,
+            textAlign: "center"
+          }}>
+            <h1
               style={{
-                fontSize: 25,
-                fontFamily: "Roboto Slab",
-                fontWeight: "Bold",
+                fontSize: 28,
+                fontFamily: 'Gilroy',
+                fontWeight: 'bold',
                 paddingTop: 15,
+                marginBottom: 15
               }}
             >
-              Transaction Tracker
-            </Typography>
-          </Grid>
-          <Grid item xs={12} style={{ textAlign: "center" }}>
+              Transaction Assist
+            </h1>
+
             <Typography
               style={{
                 fontSize: 18,
                 fontFamily: "Roboto Slab",
-                paddingTop: 10,
               }}
             >
-              Exactly know the progress of your client's Transaction!
+              Exactly know the progress of your client's transaction!
             </Typography>
           </Grid>
-          <Grid item xs={12} style={{ textAlign: "center", paddingTop: "3%" }}>
-            <ReallosButton primary buttonWidth={"32%"} onClick={closeModal}>
+
+          <Grid item style={{ textAlign: "center", paddingTop: "3%" }}>
+            <ReallosButton primary buttonWidth={"32%"} onClick={hideInitModal}>
               Continue
             </ReallosButton>
           </Grid>
@@ -152,168 +77,52 @@ function TransactionAssist(props) {
     );
   }
 
-  function closeModal() {
-    // function to close the first time modal
-    setModal(false);
+  /**
+   * Hide first-time user modal.
+   */
+  const hideInitModal = () => {
+    setInitModalVisibility(false);
   }
 
-  function displayExpansionPanels() {
-    // function to render the expansion panel
+  /**
+   * Renders acordions within the screen.
+   */
+  const displayAccordions = () => {
     return (
-      <Grid container direction="column" spacing={2}>
-        <Grid item>
-          <ExpansionPanel>
-            <ExpansionPanelSummary expandIcon={<TriangleDownIcon />}>
-              <Grid container direction="row" alignItems="center" spacing={4}>
-                <Grid item>
-                  <DotFillIcon size={23} />
-                </Grid>
-                <Divider orientation="vertical" style={{ height: 50 }} />
-                <Grid item>
-                  <Grid
-                    container
-                    direction="row"
-                    alignItems="center"
-                    spacing={3}
-                  >
-                    <Grid item>
-                      <CheckCircleIcon size={23} />
-                    </Grid>
-                    <Grid item>
-                      <Typography className="expansion-heading">
-                        Pre-Approval
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails className="exppanel">
-              {
-                <div>
-                  <div className="heading">
-                    <h1 className="title">Tasks</h1>
-                    <h6 className="track">2 of 7 tasks completed</h6>
-                    <div style={{}}>
-                      <ReallosButton
-                        primary
-                        onClick={openDrawer}
-                        className="add-task-button"
-                      >
-                        Add New Task
-                      </ReallosButton>
-                    </div>
-                  </div>
-
-                  {taskList.map((task) => {
-                    return (
-                      <ListItem
-                        className={
-                          task.completed ? "assist-task-completed" : ""
-                        }
-                      >
-                        <ListItemIcon>
-                          {task.completed ? (
-                            <CheckIcon className="checkicon-transaction" />
-                          ) : (
-                            <DotFillIcon />
-                          )}
-                        </ListItemIcon>
-                        <ListItemText>
-                          <span className="assist-task-date">{task.date}</span>
-                          {task.name}
-                        </ListItemText>
-                      </ListItem>
-                    );
-                  })}
-
-                  <div className="timeline"></div>
-                  <div className="docBox">
-                    <div className="heading-2">
-                      <h1 className="title-2">Documents</h1>
-                      <h6 className="track-2">1 of 2 completed</h6>
-                    </div>
-                    <div class="docs">
-                      <div class="doc">
-                        <img src={doc} alt="doc-svg" />
-                        <div className="doc-details">
-                          <p>Document 1</p>
-                          <p>Document is filled in</p>
-                        </div>
-                      </div>
-                      <div class="doc">
-                        <img src={doc} alt="doc-svg" />
-                        <div className="doc-details">
-                          <p>Document 1</p>
-                          <p>Document is filled in</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              }
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        </Grid>
-
-        <Grid item>
-          <ExpansionPanel>
-            <ExpansionPanelSummary expandIcon={<TriangleDownIcon />}>
-              <Grid container direction="row" alignItems="center" spacing={4}>
-                <Grid item>
-                  <DotFillIcon size={23} />
-                </Grid>
-                <Divider orientation="vertical" style={{ height: 50 }} />
-                <Grid item>
-                  <Grid
-                    container
-                    direction="row"
-                    alignItems="center"
-                    spacing={3}
-                  >
-                    <Grid item>
-                      <SearchIcon size={23} />
-                    </Grid>
-                    <Grid item>
-                      <Typography className="expansion-heading">
-                        Find an Agent
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </ExpansionPanelSummary>
-          </ExpansionPanel>
-        </Grid>
+      <Grid container direction="column" spacing={2} style={{ marginBottom: 20 }}>
+        <AssistPreApproval />
+        <AssistFindAgent />
       </Grid>
     );
   }
 
   return (
     <Scaffold navBar navRail>
-      {addTaskDrawer()}
-      {firstTimeModal()}
-      <Box component="div" paddingTop={5} paddingBottom={1}>
+      <Box component="div" paddingTop={6} paddingBottom={1}>
         <Grid
           container
           direction="row"
           alignItems="center"
           spacing={2}
-          justify="flex-start"
         >
           <Grid item>
-            <PackageIcon size={35} />
+            <PackageIcon size={28} />
           </Grid>
-          <Grid item>
-            <h2 className="transaction-heading">
-              {
-                "Transaction 1" //Name of the transaction
-              }
-            </h2>
+          <Grid item style={{
+            fontSize: 20
+          }}>
+            Transaction 1
           </Grid>
         </Grid>
-        {displayExpansionPanels()}
+        <h1 style={{
+          marginTop: 15
+        }}>
+          Transaction Assist
+        </h1>
       </Box>
+
+      {displayAccordions()}
+      {initModal()}
     </Scaffold>
   );
 }
