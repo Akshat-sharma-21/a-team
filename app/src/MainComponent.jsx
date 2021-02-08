8f952f0a362f2c309f46306a76b00e31882bcf2a
PrasannajeetBajpai <prasannajeetbajpai@gmail.com>
2021-02-08 13:15:38 +0530

    Recommended Updated

--- app/src/MainComponent.jsx
+++ app/src/MainComponent.jsx
@@ -5,6 +5,7 @@ import Document from "./components/Documents/Documents";
 import Document2 from "./components/Documents/Documents2";
 import Signin from "./components/account/SignIn/SignIn";
 import Signup from "./components/account/SignUp/SignUp";
+import Recommend from "./components/Recommened/Recommend";
 
 function Main() {
   return (
@@ -15,6 +16,7 @@ function Main() {
       <Route path="/documents2" component={Document2} />
       <Route path="/Signin" component={Signin} />
       <Route path="/Signup" component={Signup} />
+      <Route path="/Recommend" component={Recommend} />
     </Switch>
   );
 }
