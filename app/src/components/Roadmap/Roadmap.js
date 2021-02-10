5b24a1a7060012d8774e3c476333c3956ae1ddfa
Akashnemade21 <akashnemade21@gmail.com>
2021-02-10 17:47:58 +0530

    App: Roadmap edit

--- app/src/components/Roadmap/Roadmap.js
+++ app/src/components/Roadmap/Roadmap.js
@@ -13,32 +13,10 @@ import {
   Stepper,
 } from "@material-ui/core";
 import ProfilePic from "../../assets/Roadmap_img.png";
+import Scaffold from "../utilities/Scaffold/Scaffold";
 import { ArrowRightIcon, LockIcon, PencilIcon } from "@primer/octicons-react";
 
 function Roadmap() {
-  function getSteps() {
-    return [
-      "Pre-approval",
-      "Find an Agent",
-      "Make an offer",
-      "Finalize your Deal",
-      "Home Inspection",
-    ];
-  }
-
-  function getStepContent(step) {
-    switch (step) {
-      case 0:
-        return `It helps you find out how much you can afford and is the first.`;
-      case 1:
-        return "An ad group contains one or more ads which target a shared set of keywords.";
-      case 2:
-        return `Try out different ad text to see what brings in the most customers.`;
-      default:
-        return "Unknown step";
-    }
-  }
-
   function stepIcon(props) {
     const { active, completed } = props;
     return (
@@ -57,14 +35,13 @@ function Roadmap() {
   }
 
   const [activeStep, setActiveStep] = React.useState(0);
-  const steps = getSteps();
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
   return (
-    <Container className="roadmap">
+    <Scaffold>
       <Grid container direction="row" justify="center" alignItems="center">
         <Grid item xs={7}>
           <div className="roadmap-heading">Roadmap</div>
@@ -78,37 +55,124 @@ function Roadmap() {
         </Grid>
 
         <Grid item xs={12}>
-          <Stepper
-            activeStep={activeStep}
-            orientation="vertical"
-            connector={<StepConnector />}
-          >
-            {steps.map((label, index) => (
-              <Step key={label}>
-                <StepLabel StepIconComponent={stepIcon}>
-                  <div className="roadmap-subheading">{label}</div>
-                </StepLabel>
-                <StepContent>
-                  <div className="roadmap-desc">{getStepContent(index)}</div>
-                  <div>
-                    <Button className="roadmap-date" disabled>
-                      30 Sep 2020
-                    </Button>
-                    <IconButton
-                      className="roadmap-next-btn"
-                      size="small"
-                      onClick={handleNext}
-                    >
-                      <ArrowRightIcon size={24} style={{ color: "#ffffff" }} />
-                    </IconButton>
-                  </div>
-                </StepContent>
-              </Step>
-            ))}
+          <Stepper activeStep={activeStep} orientation="vertical">
+            <Step>
+              <StepLabel StepIconComponent={stepIcon}>
+                <div className="roadmap-subheading">Pre-approval</div>
+              </StepLabel>
+              <StepContent className="step-desc">
+                <div className="roadmap-desc">
+                  It helps you find out how much you can afford and is the
+                  first.
+                </div>
+                <div>
+                  <Button className="roadmap-date" disabled>
+                    30 Sep 2020
+                  </Button>
+                  <IconButton
+                    className="roadmap-next-btn"
+                    size="small"
+                    onClick={handleNext}
+                  >
+                    <ArrowRightIcon size={24} style={{ color: "#ffffff" }} />
+                  </IconButton>
+                </div>
+              </StepContent>
+            </Step>
+
+            <Step>
+              <StepLabel StepIconComponent={stepIcon}>
+                <div className="roadmap-subheading">Find an Agent</div>
+              </StepLabel>
+              <StepContent className="step-desc">
+                <div className="roadmap-desc">
+                  An ad group contains one or more ads which target a shared set
+                  of keywords.
+                </div>
+                <div>
+                  <Button className="roadmap-date" disabled>
+                    30 Sep 2020
+                  </Button>
+                  <IconButton
+                    className="roadmap-next-btn"
+                    size="small"
+                    onClick={handleNext}
+                  >
+                    <ArrowRightIcon size={24} style={{ color: "#ffffff" }} />
+                  </IconButton>
+                </div>
+              </StepContent>
+            </Step>
+
+            <Step>
+              <StepLabel StepIconComponent={stepIcon}>
+                <div className="roadmap-subheading">Make an offer</div>
+              </StepLabel>
+              <StepContent className="step-desc">
+                <div className="roadmap-desc">
+                  Try out different ad text to see what brings in the most
+                  customers.
+                </div>
+                <div>
+                  <Button className="roadmap-date" disabled>
+                    30 Sep 2020
+                  </Button>
+                  <IconButton
+                    className="roadmap-next-btn"
+                    size="small"
+                    onClick={handleNext}
+                  >
+                    <ArrowRightIcon size={24} style={{ color: "#ffffff" }} />
+                  </IconButton>
+                </div>
+              </StepContent>
+            </Step>
+
+            <Step>
+              <StepLabel StepIconComponent={stepIcon}>
+                <div className="roadmap-subheading">Finalize your Deal</div>
+              </StepLabel>
+              <StepContent className="step-desc">
+                <div className="roadmap-desc">Unknown step</div>
+                <div>
+                  <Button className="roadmap-date" disabled>
+                    30 Sep 2020
+                  </Button>
+                  <IconButton
+                    className="roadmap-next-btn"
+                    size="small"
+                    onClick={handleNext}
+                  >
+                    <ArrowRightIcon size={24} style={{ color: "#ffffff" }} />
+                  </IconButton>
+                </div>
+              </StepContent>
+            </Step>
+
+            <Step>
+              <StepLabel StepIconComponent={stepIcon}>
+                <div className="roadmap-subheading">Home Inspection</div>
+              </StepLabel>
+              <StepContent className="step-desc">
+                <div className="roadmap-desc">Unknown step</div>
+                <div>
+                  <Button className="roadmap-date" disabled>
+                    30 Sep 2020
+                  </Button>
+                  <IconButton
+                    className="roadmap-next-btn"
+                    size="small"
+                    onClick={handleNext}
+                  >
+                    <ArrowRightIcon size={24} style={{ color: "#ffffff" }} />
+                  </IconButton>
+                </div>
+              </StepContent>
+            </Step>
           </Stepper>
         </Grid>
       </Grid>
-    </Container>
+    </Scaffold>
   );
 }
 
