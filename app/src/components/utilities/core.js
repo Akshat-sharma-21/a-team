183631021c87c789b3bc7974b0bd3ac7cf7ad28f
Melvin. L. Abraham <melvinredvirus@gmail.com>
2021-02-15 12:35:14 +0530

    feat(app): Add Searchbar component

--- app/src/components/utilities/core.js
+++ app/src/components/utilities/core.js
@@ -2,10 +2,12 @@ import ModalSheet from './ModalSheet/ModalSheet';
 import ReallosButton from './reallos_button/ReallosButton';
 import Scaffold from './Scaffold/Scaffold';
 import BottomNav from './BottomNav/BottomNav';
+import SearchBar from './SearchBar/SearchBar';
 
 export {
   ModalSheet,
   ReallosButton,
   Scaffold,
-  BottomNav
+  BottomNav,
+  SearchBar,
 };
