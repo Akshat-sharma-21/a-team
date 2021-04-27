8dbd68dd217ac9ba27f67ccae9bc078a537a9b0c
Akshat Sharma <sharmaakshat212000@gmail.com>
2021-04-27 16:12:44 -0500

    Functional rendering of TasksDashboard

--- app/src/components/TasksDashboard/TasksDashboard.jsx
+++ app/src/components/TasksDashboard/TasksDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { SearchIcon } from "@primer/octicons-react";
-import { Scaffold, SearchBar } from "../utilities/core";
+import { ReallosButton, Scaffold, SearchBar } from "../utilities/core";
 import "./TasksDashboard.css";
 
 import {
@@ -37,11 +37,155 @@ function TasksDashboard(props) {
     if (props.utils.reload === true) {
       props.fetchUser();
     }
-
-    setTasksList(props.roadmap.PreApproval.Tasks);
   }, []);
 
-  console.log(props.roadmap);
+  if (props.utils.reload === false && tasksList === null) {
+    // If the user has not been loaded and the tasklist is not set
+    let addArray = [];
+
+    if (props.roadmap.PreApproval.Tasks.length !== 0) {
+      // if PreApproval has Tasks
+      props.roadmap.PreApproval.Tasks.forEach((val) => {
+        if (val.to === "user") addArray.push(val);
+      });
+    }
+
+    if (props.roadmap.FindAgent.Tasks.length !== 0) {
+      // if FindAgent has Tasks
+      props.roadmap.FindAgent.Tasks.forEach((val) => {
+        if (val.to === "user") addArray.push(val);
+      });
+    }
+
+    if (props.roadmap.FindHome.Tasks.length !== 0) {
+      // if FindHome has Tasks
+      props.roadmap.FindHome.Tasks.forEach((val) => {
+        if (val.to === "user") addArray.push(val);
+      });
+    }
+
+    if (props.roadmap.HomeInspection.Tasks.length !== 0) {
+      // if HomeInspection has Tasks
+      props.roadmap.HomeInspection.Tasks.forEach((val) => {
+        if (val.to === "user") addArray.push(val);
+      });
+    }
+
+    if (props.roadmap.EscrowTitle.Tasks.length !== 0) {
+      // if EscrowTitle has Tasks
+      props.roadmap.EscrowTitle.Tasks.forEach((val) => {
+        if (val.to === "user") addArray.push(val);
+      });
+    }
+
+    if (props.roadmap.HomeInsurance.Tasks.length !== 0) {
+      // if HomeInsuramnce has Tasks
+      props.roadmap.HomeInsurance.Tasks.forEach((val) => {
+        if (val.to === "user") addArray.push(val);
+      });
+    }
+
+    if (props.roadmap.Closing.Tasks.length !== 0) {
+      // if Closing has Tasks
+      props.roadmap.Closing.Tasks.forEach((val) => {
+        if (val.to === "user") addArray.push(val);
+      });
+    }
+
+    setTasksList(addArray);
+  }
+
+  function displayDate(date) {
+    // To display the date in the required format
+    let newDate = new Date(date.seconds * 1000);
+    let month = null;
+    switch (newDate.getMonth()) {
+      case 0:
+        month = "Jan";
+        break;
+      case 1:
+        month = "Feb";
+        break;
+      case 2:
+        month = "March";
+        break;
+      case 3:
+        month = "April";
+        break;
+      case 4:
+        month = "May";
+        break;
+      case 5:
+        month = "June";
+        break;
+      case 6:
+        month = "July";
+        break;
+      case 7:
+        month = "Aug";
+        break;
+      case 8:
+        month = "Sept";
+        break;
+      case 9:
+        month = "Oct";
+        break;
+      case 10:
+        month = "Nov";
+        break;
+      case 11:
+        month = "Dec";
+        break;
+      default:
+        month = "";
+        break;
+    }
+    return `${newDate.getDate()} ${month}`;
+  }
+
+  function displayButton(type) {
+    // To display the required buttons
+    if (type === "documents") {
+      return (
+        <div className="tasks-dashboard-task-list-item-actions-group">
+          <ReallosButton
+            dense
+            fullWidth
+            variant="primary"
+            primary={true}
+            onClick={() => {
+              window.location.href = "/documents"; // Add the id of the document here too
+            }}
+          >
+            Upload
+          </ReallosButton>
+        </div>
+      );
+    } else {
+      return (
+        <div className="tasks-dashboard-task-list-item-actions-group">
+          <ReallosButton
+            dense
+            fullWidth
+            variant="primary"
+            primary={true}
+            onClick={() => {}}
+          >
+            Mark Completed
+          </ReallosButton>
+          <ReallosButton
+            dense
+            fullWidth
+            variant="primary"
+            primary={false}
+            onClick={() => {}}
+          >
+            Help
+          </ReallosButton>
+        </div>
+      );
+    }
+  }
 
   const PrimaryContent = () => {
     if (tasksList === null || filteredList === null) {
@@ -99,11 +243,9 @@ function TasksDashboard(props) {
         </div>
       );
     } else {
-      // If documents are present to be rendered
+      // If Tasks are present to be rendered
       return (
         <div style={{ width: "100%" }}>
-          <div className="tasks-dashboard-section-title">Preapproval</div>
-
           <List className="tasks-dashboard-list">
             {filteredList.map((task, index) => (
               <Accordion key={index} className="tasks-dashboard-list-item">
@@ -112,7 +254,7 @@ function TasksDashboard(props) {
                   expandIcon={<ExpandMoreIcon />}
                 >
                   <div className="tasks-dashboard-accordion-date">
-                    {task.Date}
+                    {displayDate(task.date)}
                   </div>
 
                   <div className="tasks-dashboard-list-item-header-text-group">
@@ -120,36 +262,22 @@ function TasksDashboard(props) {
                       noWrap
                       className="tasks-dashboard-list-item-heading"
                     >
-                      {task.Title}
+                      {task.title}
                     </Typography>
                     <Typography
                       noWrap
                       className="tasks-dashboard-list-item-subheading"
                     >
-                      {task.Description}
+                      {task.description}
                     </Typography>
                   </div>
                 </AccordionSummary>
 
                 <AccordionDetails className="tasks-dashboard-task-list-item-details">
                   <Typography className="tasks-dashboard-task-list-item-details-desc">
-                    {task.Description}
+                    {task.description}
                   </Typography>
-
-                  <div className="tasks-dashboard-task-list-item-actions-group">
-                    {/* {task.Actions.map((taskAction, index) => (
-                      <ReallosButton
-                        key={taskAction.label}
-                        dense
-                        fullWidth
-                        variant="primary"
-                        primary={index === 0}
-                        onClick={taskAction.onClick}
-                      >
-                        {taskAction.label}
-                      </ReallosButton>
-                    ))} */}
-                  </div>
+                  {displayButton(task.type)}
                 </AccordionDetails>
               </Accordion>
             ))}
