5072db43daedbe36e0836d04b51ed40be471a331
Akshat Sharma <sharmaakshat212000@gmail.com>
2021-06-11 14:18:17 -0500

    questionnaire assets changed + minor fixes in css

--- app/src/components/questionnaire/QuestionnaireOutro.jsx
+++ app/src/components/questionnaire/QuestionnaireOutro.jsx
@@ -24,7 +24,7 @@ function QuestionnaireOutro(props) {
               fontSize: 20,
             }}
           >
-            Selecting an Ideal Agent for you
+            Loading...
           </div>
         </div>
       ) : (
